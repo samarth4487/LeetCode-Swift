7a7d8ac0c8b3faa2501ce966c5aa3b6f09c12fdb
Samarth Paboowal <samarthpaboowal@yahoo.co.in>
2022-01-19 22:26:24 +0530

    Update ReplaceElementsWithGreatestElementOnRightSide.swift

--- ReplaceElementsWithGreatestElementOnRightSide.swift
+++ ReplaceElementsWithGreatestElementOnRightSide.swift
@@ -1,3 +1,4 @@
+// Looping from left to right
 class Solution {
     func replaceElements(_ arr: [Int]) -> [Int] {
         guard arr.count > 1 else {
@@ -17,6 +18,30 @@ class Solution {
         }
 
         array[array.count - 1] = -1
+
+        return array
+    }
+}
+
+// Looping from right to left (Better solution)
+class Solution {
+    func replaceElements(_ arr: [Int]) -> [Int] {
+        guard arr.count > 1 else {
+            return [-1]
+        }
+
+        var array = Array(repeating: -1, count: arr.count)
+        var index = array.count - 1
+        var max = -1
+        
+        while index > 0 {
+            if arr[index] > max {
+                max = arr[index]
+            }
+            
+            array[index - 1] = max
+            index -= 1
+        }
 
         return array
     }
